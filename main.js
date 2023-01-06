d969e013473c774252ea70f8a3c2e26e94a60c72
abdssamad <zianiabdssamad30@gmail.com>
2023-01-06 16:17:01 +0100

    done

--- main.js
+++ main.js
@@ -105,7 +105,7 @@ function getQuestions()
   }
 
 
-  myrequest.open('GET','https://abdessamadziani.github.io/quizz/questions.json',true)
+  myrequest.open('GET','https://abdessamadziani.github.io/quizz-app/questions.json',true)
   myrequest.send()
 
 }
@@ -313,4 +313,3 @@ function randoom(q_count,questions){
   }
 }
 
-console.log(array)
